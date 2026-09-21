68999fa133af7d9930500e93a1c473799d666c0a
Swapnil <swpnilsupekar@gmail.com>
2026-09-21 11:01:11 +0530

    ADD model

---
+++ src/com/employee/main/Main.java
@@ -0,0 +1,14 @@
+package com.employee.main;
+
+import com.employee.model.Deparment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Deparment d=new Deparment();
+		
+
+	}
+
+}
---
+++ src/com/employee/model/Deparment.java
@@ -0,0 +1,48 @@
+package com.employee.model;
+
+public class Deparment {
+
+	private int dept_Id;
+	private String dept_Name;
+	private String dept_Desc;
+
+	public Deparment() {
+
+	}
+
+	public Deparment(String dept_Name, String dept_Desc) {
+		this.dept_Name = dept_Name;
+		this.dept_Desc = dept_Desc;
+	}
+
+	public Deparment(int dept_Id, String dept_Name, String dept_Desc) {
+		this.dept_Id = dept_Id;
+		this.dept_Name = dept_Name;
+		this.dept_Desc = dept_Desc;
+	}
+
+	public int getDept_Id() {
+		return dept_Id;
+	}
+
+	public void setDept_Id(int dept_Id) {
+		this.dept_Id = dept_Id;
+	}
+
+	public String getDept_Name() {
+		return dept_Name;
+	}
+
+	public void setDept_Name(String dept_Name) {
+		this.dept_Name = dept_Name;
+	}
+
+	public String getDept_Desc() {
+		return dept_Desc;
+	}
+
+	public void setDept_Desc(String dept_Desc) {
+		this.dept_Desc = dept_Desc;
+	}
+
+}
